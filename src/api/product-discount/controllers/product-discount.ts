d022a6bdb3000413a6df812d1eb4db214e149a03
alex <arevaloalex9@gmail.com>
2022-08-06 16:46:38 -0500

    create db-models

---
+++ src/api/product-discount/controllers/product-discount.ts
@@ -0,0 +1,7 @@
+/**
+ *  product-discount controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::product-discount.product-discount');
---
+++ src/api/product-discount/services/product-discount.ts
@@ -0,0 +1,7 @@
+/**
+ * product-discount service.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::product-discount.product-discount');
